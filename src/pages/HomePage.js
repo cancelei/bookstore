99fc6df59f9137366941b033364b92a915c04718
Glauber <glauber.bannwart@gmail.com>
2023-07-21 14:49:38 -0300

    Homepage update

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,32 @@
-// HomePage.js
-import Book from "../components/Book";
-import BookList from "../components/BookList";
-import CreateBook from "../components/CreateBook";
+import React, { useState } from 'react';
+import BookList from '../components/BookList';
+import CreateBook from '../components/CreateBook';
 
 function HomePage() {
+  const [books, setBooks] = useState([
+    { id: 1, title: 'Book 1', author: 'Author 1' },
+    { id: 2, title: 'Book 2', author: 'Author 2' },
+    // initial books
+  ]);
+
+  const addBook = (title, author) => {
+    // create a new book object
+    const newBook = {
+      id: books.length + 1, // generate a new id
+      title,
+      author,
+    };
+
+    // add the new book to the books array
+    setBooks([...books, newBook]);
+  };
+
   return (
     <>
       <h1>Home Page</h1>
       <p>This is the home page with books!</p>
-      <BookList />
-      <Book />
-      <CreateBook/>
+      <BookList books={books} />
+      <CreateBook onCreate={addBook} />
     </>
   );
 }
